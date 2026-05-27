18058de3e121e2073c94fe9c7fc033d664220722
Alondra <alobuuls@gmail.com>
2026-05-26 19:00:16 -0500

    ✨ feat: implement reusable localStorage service with reactive forms persistence

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+import { LocalStorageService } from './storage/local-storage.service';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+	selector: 'app-root',
+	templateUrl: './app.component.html',
+	styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular-local-session-storage';
+	constructor(private _formBuilder: FormBuilder, private _localStorage: LocalStorageService) {}
+
+	formGroup = this._formBuilder.nonNullable.group({
+		names: [''],
+		claim: ['']
+	});
+
+	formDataInStorage?: IForm;
+
+	clickSend(): void {
+		this._localStorage.setItem('form', JSON.stringify(this.formGroup.value));
+
+		const formStorage = JSON.parse(localStorage.getItem('form')!) as IForm;
+		this.formDataInStorage = formStorage!;
+	}
+}
+interface IForm {
+	names: '';
+	claim: '';
 }
